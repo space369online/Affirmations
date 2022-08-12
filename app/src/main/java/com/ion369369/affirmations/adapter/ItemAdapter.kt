2118f303f9b10be78759e30d577ab75f28715215
Ion Popescu <ion369369@gmail.com>
2022-08-13 00:32:00 +0300

    Adding images to the list items. Polishing the UI.

--- app/src/main/java/com/ion369369/affirmations/adapter/ItemAdapter.kt
+++ app/src/main/java/com/ion369369/affirmations/adapter/ItemAdapter.kt
@@ -4,27 +4,31 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.ion369369.affirmations.R
 import com.ion369369.affirmations.model.Affirmation
 
-class ItemAdapter (
+class ItemAdapter(
     private val context: Context,
     private val dataset: List<Affirmation>
-    ): RecyclerView.Adapter<ItemAdapter.ItemViewHolder>() {
-    class ItemViewHolder(view: View): RecyclerView.ViewHolder(view) {
+) : RecyclerView.Adapter<ItemAdapter.ItemViewHolder>() {
+    class ItemViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         val textView: TextView = view.findViewById(R.id.item_title)
+        val imageViewHolder: ImageView = view.findViewById(R.id.item_image)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
-        val adapterLayout = LayoutInflater.from(parent.context).inflate(R.layout.list_item, parent, false)
+        val adapterLayout =
+            LayoutInflater.from(parent.context).inflate(R.layout.list_item, parent, false)
         return ItemViewHolder(adapterLayout)
     }
 
     override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
         val item = dataset[position]
         holder.textView.text = context.resources.getString(item.stringResourceId)
+        holder.imageViewHolder.setImageResource(item.imageResourceId)
     }
 
     override fun getItemCount(): Int {
